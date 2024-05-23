a129a7783068c752203857635dafc8297c971a4a
Maciej Waligóra <100129859+MaciejWaligora@users.noreply.github.com>
2024-05-23 12:55:14 +0200

    init

---
+++ Signal.ts
@@ -0,0 +1,23 @@
+
+export class Signal<T>{
+
+    private _listeners: [Object, (data?:T)=>void ][] = []
+
+    public addListener(listener: (data?:T)=>void, context: Object){
+        const boundListener = listener.bind(context);
+        this._listeners.push([context, boundListener]);
+    }
+
+    public removeListener(listener: ()=>void, context: Object){
+        const index = this._listeners.findIndex(([ctx, fn]) => ctx === context && fn === listener);
+        if (index !== -1) {
+            this._listeners.splice(index, 1);
+        }
+    }
+
+    public emit(data?: T){
+        for(const listener of this._listeners){
+            listener[1](data);
+        }
+    }
+}
